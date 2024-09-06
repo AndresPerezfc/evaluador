37c5719eab78d1aa058943b758207271d76bbd9b
andresperezfc <felipe3apc@gmail.com>
2024-09-05 21:37:42 -0500

    Finalización de las migraciones

--- database/migrations/2024_09_05_214502_create_criterios_table.php
+++ database/migrations/2024_09_05_214502_create_criterios_table.php
@@ -11,8 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('critorios', function (Blueprint $table) {
+        Schema::create('criterios', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre');
+            $table->string('descripción');
+            $table->integer('puntaje_maximo');
             $table->timestamps();
         });
     }
@@ -22,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('critorios');
+        Schema::dropIfExists('criterios');
     }
 };
--- database/migrations/2024_09_05_214714_create_evaluations_table.php
+++ database/migrations/2024_09_05_214714_create_evaluations_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,11 @@ return new class extends Migration
     {
         Schema::create('evaluations', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('innovation_id')->constrained('innovations');
+            $table->foreignId('criterio_id')->constrained('criterios');
+            $table->integer('puntaje')->default(0);
+            $table->text('comentario')->nullable();
             $table->timestamps();
         });
     }
