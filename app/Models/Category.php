e5fe04f33dcf2b0630ee687ff55a3ab011c48379
andresperezfc <felipe3apc@gmail.com>
2024-09-13 10:47:19 -0500

    Completado el modelo Category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    // Definir los campos asignables
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+
+    // Relación uno a muchos: una categoría tiene muchas innovaciones
+    public function innovations()
+    {
+        return $this->hasMany(Innovation::class);
+    }
 }
